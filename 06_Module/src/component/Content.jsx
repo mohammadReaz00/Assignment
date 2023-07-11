955ac290e60eb6d3a1444965765c6b5b3a64547b
MohammadReaz <mdreaz549@gmail.com>
2023-07-11 15:04:20 +0600

    modified module 6

--- 06_Module/src/component/Content.jsx
+++ 06_Module/src/component/Content.jsx
@@ -1,30 +1,19 @@
-import { useState } from "react";
-
 function Content() {
-  const [displayText, setDisplayText] = useState("");
-  const [inputText, setInputText] = useState("");
-
   const handleButtonClick = () => {
-    setDisplayText(inputText);
-  };
-
-  const handleInputChange = (event) => {
-    setInputText(event.target.value);
+    const inputText = document.getElementById("inputText").value;
+    const displayArea = document.getElementById("displayArea");
+    displayArea.innerText = inputText;
   };
 
   return (
     <div className="flex-grow">
       <div className="flex flex-col items-center p-4">
-        <textarea
-          className="border-2 p-2 mb-2 w-2/4 h-32"
-          onChange={handleInputChange}
-          value={inputText}
-        ></textarea>
+        <textarea id="inputText" className="border-2 p-2 mb-2 w-2/4 h-32"></textarea>
         <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleButtonClick}>
           Display Text
         </button>
       </div>
-      {displayText && <pre className="mt-4 whitespace-pre-wrap">{`${displayText}`}</pre>}
+      <div id="displayArea" className="mt-4 whitespace-pre-wrap"></div>
     </div>
   );
 }
--- 06_Module/src/component/Footer.jsx
+++ 06_Module/src/component/Footer.jsx
@@ -1,8 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 function Footer() {
   return (
     <div>
       <footer className="bg-gray-200 p-4 text-center">
-        <p>Created By Mohammad Reaz</p>
+        <p className="text-gray-600">
+          Created By <span className="text-purple-500">Mohammad Reaz</span>
+        </p>
+        <p className="text-sm mt-2">
+          <span className="text-gray-500">Follow me on:</span>{" "}
+          <a
+            href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-purple-500"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-purple-500"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://www.linkedin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-purple-500"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </p>
       </footer>
     </div>
   );
